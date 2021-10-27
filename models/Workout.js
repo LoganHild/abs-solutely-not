f3872be3aec9eb9d95030ce86af7222cdcd667f0
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-10-27 16:47:35 -0500

    added model for Workout

--- models/Workout.js
+++ models/Workout.js
@@ -1,16 +1,51 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-    string: {
-        type: String,
-        trim: true,
-        required: "String is Required"
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
     },
+    
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+                require: true,
+                trim: true,
+            },
+            duration: {
+                type: Number,
+                required: true,
+            },
+            distance: {
+                type: Number,
+                required: true,
+            },
+            weight: {
+                type: Number,
+                required: true,
+            },
+            reps: {
+                type: Number,
+                required: true,
+            },
+            sets: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+});
 
-    number: {
-        type: Number,
-        unique: true,
-        required: true
-    }
-})
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
