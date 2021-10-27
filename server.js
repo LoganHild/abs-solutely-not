ea5e7f87f9f39903f67ac00248eeef342849b47f
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-10-27 16:34:56 -0500

    added server file

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const path = require('path');
 const express = require('express');
 const mongojs = require("mongojs");
-// const routes = require('./controllers');
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+require('dotenv').config();
+
+const db = require("./models");
 
 const app = express();
 
@@ -9,10 +13,25 @@ app.use(logger("dev"));
 
 const PORT = process.env.PORT || 3001;
 
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static("public"));
+
+mongoose.connect(
+    PROCESS.ENV.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+require('./routes/api-routes')(app);
+require('./routes/html-ruotes')(app);
+
+app.listen(PORT, () => {
+    console.log(`App running at http://localhost:${PORT}`);
+});
 
