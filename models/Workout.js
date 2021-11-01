92e6b70be67d84fba113585e87d331b685642e42
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-11-01 18:39:18 -0500

    added function for duration

--- models/Workout.js
+++ models/Workout.js
@@ -47,5 +47,12 @@ const workoutSchema = new Schema({
     },
 });
 
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+      console.log(total, exercise);
+      return total + exercise.duration;
+    }, 0);
+  });
+
 const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
